8e4200c51f326f83e6619362fbfb20d9150960f0
Surfpeny420 <surfpeny@gmail.com>
2026-04-21 15:53:41 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 🔥 КОНФИГ ИЗ FIREBASE CONSOLE
 const firebaseConfig = {
-  apiKey: "AIzaSyB-YsuCz5Jeb_9h61B2mDyXqv7KmWIzz_8",
-  authDomain: "model-school-app-6d750.firebaseapp.com",
-  projectId: "model-school-app-6d750",
-  storageBucket: "model-school-app-6d750.firebasestorage.app",
-  messagingSenderId: "332990568572",
-  appId: "1:332990568572:web:2bdb202b097e1df11b11f9",
+    apiKey: "ВАШ_API_KEY",
+    authDomain: "ВАШ_AUTH_DOMAIN",
+    projectId: "ВАШ_PROJECT_ID",
+    storageBucket: "ВАШ_STORAGE_BUCKET",
+    messagingSenderId: "ВАШ_SENDER_ID",
+    appId: "ВАШ_APP_ID"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Глобальные переменные
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+console.log("Firebase initialized");
